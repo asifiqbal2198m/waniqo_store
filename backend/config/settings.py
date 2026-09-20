b57b41241068575e4387d261999cf901bb986811
asif <iasif2198@gmail.com>
2026-09-20 12:24:19 +0530

    Fix staticfiles storage backend to prevent whitenoise manifest crashes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,7 +132,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 # Media files (Uploaded images)
 MEDIA_URL = '/media/'
